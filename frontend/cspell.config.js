06035cdc0b251866f924234d465e660989ded6b6
Yoshida Takuto <takuto.yoshida@gmail.com>
2022-01-12 22:44:03 +0900

    cspellを追加

---
+++ frontend/cspell.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  language: 'en',
+  ignorePaths: [
+    'node_modules/**',
+    'package-lock.json',
+    'dist/**',
+    'build/**',
+  ],
+  ignoreWords: [
+    'eslintignore',
+    'prettierignore',
+    'loglevel',
+    'browserslist',
+    // author
+    'Takt29',
+    // libraries
+    'clsx',
+    'pmmmwh',
+  ]
+}
