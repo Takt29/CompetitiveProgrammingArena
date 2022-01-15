b7b8536d8cdf2c5e74dbaa2fde2cd0265224d055
Yoshida Takuto <takuto.yoshida@gmail.com>
2022-01-15 16:26:03 +0900

    コンテスト開催時刻を表示するようにした

--- frontend/src/page/ContestPage.tsx
+++ frontend/src/page/ContestPage.tsx
@@ -2,18 +2,22 @@ import {
   Box,
   Container,
   Heading,
+  Stack,
   Tab,
   TabList,
   TabPanel,
   TabPanels,
   Tabs,
+  Text,
 } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { ContestInfoTab } from "../component/contest/tab/ContestInfoTab";
 import { ContestStandingsTab } from "../component/contest/tab/ContestStandingsTab";
 import { ContestSubmissionsTab } from "../component/contest/tab/ContestSubmissionsTab";
 import { ContestTasksTab } from "../component/contest/tab/ContestTasksTab";
+import { ContestEndAt } from "../consumer/contest/ContestEndAt";
 import { ContestName } from "../consumer/contest/ContestName";
+import { ContestStartAt } from "../consumer/contest/ContestStartAt";
 import { ContestProvider } from "../hook/context/ContestContext";
 import { useFetchContest } from "../hook/firebase/contest";
 
@@ -26,9 +30,22 @@ export const ContestPage = () => {
   return (
     <ContestProvider value={contest}>
       <Container maxWidth={"container.lg"} marginTop={4} marginBottom={4}>
-        <Heading as={"h3"} size="lg" marginBottom={4}>
-          <ContestName />
-        </Heading>
+        <Box marginBottom={4}>
+          <Stack
+            direction={["column", "row"]}
+            spacing={4}
+            alignItems={["flex-start", "flex-end"]}
+          >
+            <Heading as={"h3"} size="lg">
+              <ContestName />
+            </Heading>
+            <Text fontSize="sm" as="div">
+              <ContestStartAt />
+              {"~"}
+              <ContestEndAt />
+            </Text>
+          </Stack>
+        </Box>
 
         <Tabs>
           <Box overflowX={"auto"}>
