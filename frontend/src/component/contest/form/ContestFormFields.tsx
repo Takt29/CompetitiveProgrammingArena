b0cc1244920f35fb6bd7e9ecee705cd399de35bf
Yoshida Takuto <takuto.yoshida@gmail.com>
2023-03-24 21:09:34 +0900

    コンテスト開始時刻、終了時刻の入力を分数で入力できるようにした

--- frontend/src/component/contest/form/ContestFormFields.tsx
+++ frontend/src/component/contest/form/ContestFormFields.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   FormControl,
   FormHelperText,
   FormLabel,
@@ -10,11 +11,14 @@ import {
   NumberInputStepper,
   Select,
   Textarea,
+  Text,
 } from "@chakra-ui/react";
+import dayjs from "dayjs";
 import { useFormContext } from "react-hook-form";
 import { Pattern } from "../../../constant/Pattern";
 import { StandingsSystemType } from "../../../constant/StandingsSystem";
 import { StandingsSystemName } from "../../../constant/StandingsSystemName";
+import { formatDateTime } from "../../../helper/dateTime";
 
 export type ContestFormFieldsData = {
   name: string;
@@ -27,10 +31,25 @@ export type ContestFormFieldsData = {
   };
 };
 
+const StartAtAutoFillItems = [
+  ["now", 0],
+  ["in 5 minutes", 5],
+  ["in 10 minutes", 10],
+] as const;
+
+const EndAtAutoFillItems = [
+  ["60 minutes", 60],
+  ["100 minutes", 100],
+  ["120 minutes", 120],
+  ["300 minutes", 300],
+] as const;
+
 export const ContestFormFields = () => {
   const {
     register,
     formState: { errors },
+    setValue,
+    getValues,
   } = useFormContext<ContestFormFieldsData>();
 
   return (
@@ -46,22 +65,70 @@ export const ContestFormFields = () => {
       <FormControl isInvalid={!!errors.startAt}>
         <FormLabel htmlFor="startAt">startAt</FormLabel>
         <Input
+          placeholder="YYYY/MM/DD hh:mm"
           {...register("startAt", {
             required: true,
             pattern: Pattern.DATE_TIME,
           })}
         />
-        <FormHelperText>YYYY/MM/DD hh:mm</FormHelperText>
+        <FormHelperText>
+          <Text as="span" marginRight={2}>
+            Auto Fill:
+          </Text>
+          {StartAtAutoFillItems.map(([text, mins]) => (
+            <Box
+              key={text}
+              as="button"
+              textDecorationLine="underline"
+              marginRight={2}
+              onClick={() =>
+                setValue(
+                  "startAt",
+                  formatDateTime(
+                    dayjs().add(mins, "minutes"),
+                    "YYYY/MM/DD HH:mm"
+                  )
+                )
+              }
+            >
+              {text}
+            </Box>
+          ))}
+        </FormHelperText>
       </FormControl>
       <FormControl isInvalid={!!errors.endAt}>
         <FormLabel htmlFor="endAt">endAt</FormLabel>
         <Input
+          placeholder="YYYY/MM/DD hh:mm"
           {...register("endAt", {
             required: true,
             pattern: Pattern.DATE_TIME,
           })}
         />
-        <FormHelperText>YYYY/MM/DD hh:mm</FormHelperText>
+        <FormHelperText>
+          <Text as="span" marginRight={2}>
+            Auto Fill:
+          </Text>
+          {EndAtAutoFillItems.map(([text, duration]) => (
+            <Box
+              key={text}
+              as="button"
+              textDecorationLine="underline"
+              marginRight={2}
+              onClick={() =>
+                setValue(
+                  "endAt",
+                  formatDateTime(
+                    dayjs(getValues()["startAt"]).add(duration, "minutes"),
+                    "YYYY/MM/DD HH:mm"
+                  )
+                )
+              }
+            >
+              {text}
+            </Box>
+          ))}
+        </FormHelperText>
       </FormControl>
       <FormControl isInvalid={!!errors.rule?.system}>
         <FormLabel htmlFor="rule_system">Standings System</FormLabel>
